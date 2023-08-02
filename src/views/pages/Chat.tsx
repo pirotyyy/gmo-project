c6da6f7b70a5dca778e7306ff0d97929773d0177
ryuheisato <ryuheisato7@gmail.com>
2023-08-02 14:15:22 +0900

    reducer頑張った

--- src/views/pages/Chat.tsx
+++ src/views/pages/Chat.tsx
@@ -1,22 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { useState, ChangeEvent, FormEvent } from 'react';
 import { chat } from '../../chatgpt';
+import { setResponseText } from '../../responseTextSlice';
+import { RootState } from '../../store';
+import { useNavigate } from 'react-router-dom';
 
 const Chat = () => {
   const defaultMessage =
     '以下の要件定義を、目的、期限、アプリ概要に分けて全てtext項目としてJSON形式で出力して下さい。オブジェクトは一つにして入れ子構造にはしないでください。項目がない場合は空欄として出力して下さい。';
   const [message, setMessage] = useState<string>('');
-  const [answer, setAnswer] = useState<string>('');
-
   const handleMessageChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(event.target.value);
   };
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const useSelectorResponseText = (state: RootState) => state.responseText.value;
+  const responseText = useSelector(useSelectorResponseText);
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const responseText = await chat(defaultMessage + message);
-    setAnswer(responseText);
+    dispatch(setResponseText(responseText));
     console.log(responseText);
+    navigate("/confirm")
   };
 
   return (
@@ -34,10 +42,10 @@ const Chat = () => {
           <button type='submit'>質問する</button>
         </div>
       </form>
-      {answer && (
+      {responseText && (
         <div>
           <h2>回答:</h2>
-          <p>{answer}</p>
+          <p>{responseText}</p>
         </div>
       )}
     </div>
--- src/views/pages/ClientConfirmPage/ClientConfirmPage.tsx
+++ src/views/pages/ClientConfirmPage/ClientConfirmPage.tsx
@@ -1,10 +1,17 @@
 import ResponsiveAppBar from "../../molecules/Hedder.tsx"
 import Paper from '@mui/material/Paper';
 import "./ClientConfirmPage.css"
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+import { useEffect } from "react";
 //import {useState} from "react";
 
 const ClientConfirmPage = () => {
   //const [text,setText]=useState("");
+  const responseText = useSelector((state: RootState) => state.responseText.value);
+  useEffect(() => {
+    console.log(responseText);
+  }, [responseText]);
 
   // ChatGptから分割された値を入れる
   return (
